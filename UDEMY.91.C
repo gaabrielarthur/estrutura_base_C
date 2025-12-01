e19b14f936baddb0afddafa55448eb5440ca14b6
Gabriel.arthur <gabrielarthur01mg@gmail.com>
2025-12-01 20:22:12 -0300

    Add files via upload

---
+++ UDEMY.91.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void imprimeVetor(int *Vetor, int tamanho){
+    int i;
+    
+    for(i = 0; i < 3; i++){
+        printf("%d\n", vetor[i];)
+    }
+}
+
+
+int main(){
+  int v[3] = {1,2,3};
+
+
+  imprimeVetor(v);
+}
